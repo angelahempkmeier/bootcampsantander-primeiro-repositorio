bf44025afe5ea7636176e404df183f6fd9e2f609
aggsdp <aggsdpz@gmail.com>
2022-07-19 15:02:51 -0300

    empty, null e undefined

---
+++ variaveisTiposJS/index16.js
@@ -0,0 +1,11 @@
+abacaxi
+typeof abacaxi //retorna undefined pois nao foi declarada. bem visivel no nodeJS
+
+let abacaxi //tb dá undefined pois nao foi atribuido valor a ela
+
+let maca = "" //retorna um empty value - é uma string
+
+//fazendo
+
+maca = nulll
+//console.log(typeof(maca))
