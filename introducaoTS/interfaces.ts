39dcc64299eaf293d181754be3109c6b9b19416c
aggsdp <aggsdpz@gmail.com>
2022-07-22 16:07:59 -0300

    interfaces

---
+++ introducaoTS/interfaces.ts
@@ -0,0 +1,24 @@
+interface IAnimal {
+    nome: string
+    tipo: 'terrestre' | 'aquático'
+    executarRugido(alturaEmDecibeis: number): void
+}
+
+interface iFelino extends IAnimal{
+    visaoNoturna: boolean
+}
+
+const animal: IAnimal = {
+    //ctrl + barra para mostrar tipos possiveis
+    nome: 'Elefante',
+    tipo: 'terrestre' //nao posso colocar valor que nao foram definidos na interface
+    executarRugido(alturaEmDecibeis) => (`${alturaEmDecibeis}`)
+}
+
+animal.executarRugido('s') //aceita apenas number
+
+const felino: iFelino = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true
+}
